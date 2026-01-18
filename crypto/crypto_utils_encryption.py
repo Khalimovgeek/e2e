f443933a60b6e662085d9d7a7a66df7b745b1a52
Khalimovgeek <j94341918@gmail.com>
2026-01-18 12:38:58 +0530

    message encrytpion

---
+++ crypto/crypto_utils_encryption.py
@@ -0,0 +1,17 @@
+from cryptography.hazmat.primitives.ciphers.aead import  AESGCM
+import os, base64
+
+def encrypt_message(key: bytes, plaintext: bytes):
+    aesgcm = AESGCM(key)
+    nonce  = os.urandom(12)
+    ciphertext = aesgcm.encrypt(nonce,
+                                data=plaintext,
+                                associated_data=None
+    )
+
+    return {
+        "ciphertext": 
+    base64.b64encode(ciphertext).decode(),
+        "nonce":
+    base64.b64encode(nonce).decode()
+    }
