2c2dd2cee79adb5c3e08492c73b4e4892656401a
Khalimovgeek <j94341918@gmail.com>
2026-01-18 16:17:12 +0530

    session key funtion, testing the encryption

--- crypto/crypto_utils_keys.py
+++ crypto/crypto_utils_keys.py
@@ -1,4 +1,4 @@
-from nacl.public import PrivateKey
+from nacl.public import PrivateKey,PublicKey,Box
 from nacl.signing import SigningKey
 import base64
 
@@ -7,17 +7,27 @@ def generate_x25519_keypair():
     public = private.public_key
 
 
-    return (
-        base64.b64encode(bytes(private)).decode(),
-        base64.b64encode(bytes(public)).decode()
-    )
+    return {
+        "private": base64.b64encode(bytes(private)).decode(),
+        "public": base64.b64encode(bytes(public)).decode()
+    }
+
+def generate_session_key(sender_private_b64,peer_public_b64):
+    sender_private = PrivateKey(base64.b64decode(sender_private_b64))
+    peer_public    =  PublicKey(base64.b64decode(peer_public_b64))
+
+    box = Box(sender_private,peer_public)
+
+    return box.shared_key()
+
 
 def generate_e25519_keypair():
 
     signing_key = SigningKey.generate()
     verify_key  = signing_key.verify_key
 
-    return (
-        base64.b64encode(bytes(signing_key)).decode(),
-        base64.b64encode(bytes(verify_key)).decode()
-    )
+    return {
+            "private": base64.b64encode(bytes(signing_key)).decode(),
+            "public": base64.b64encode(bytes(verify_key)).decode()
+        }
+
--- crypto/test.py
+++ crypto/test.py
@@ -1,15 +1,24 @@
-from crypto_utils_keys import generate_e25519_keypair,generate_x25519_keypair
+from crypto_utils_keys import generate_e25519_keypair,generate_x25519_keypair,generate_session_key
 from crypto_utils_encryption import encrypt_message
 from crypto_utils_decryption import decrypt_message
 import os
 
 
-session_key = os.urandom(32)
+client_1 = generate_x25519_keypair()
+client_2 = generate_x25519_keypair()
+
+session_key_1 = generate_session_key(client_1["private"],client_2["public"])
+session_key_2 = generate_session_key(client_2["private"],client_1["public"])
+
+assert session_key_1 == session_key_2
+
+print("✅ Session keys match")
+
 
 msg ="hi".encode("utf-8")
 
-encrypted = encrypt_message(session_key,msg)
+encrypted = encrypt_message(session_key_1,msg)
 print(encrypted)
-decrypted = decrypt_message(session_key,encrypted["ciphertext"],encrypted["nonce"])
+decrypted = decrypt_message(session_key_2,encrypted["ciphertext"],encrypted["nonce"])
 
 print(decrypted)
